7f3f34e25470c517b07eb7f2b98e794bfe6d2752
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-08-03 13:41:17 -0300

    feat: Header funcional que encaminha o usuário para a rota correta.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import styles from "./styles.module.scss";
+
+import { SignInButton } from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
+
+export function Header() {
+
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image width="110" height="31" src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    );
+}
